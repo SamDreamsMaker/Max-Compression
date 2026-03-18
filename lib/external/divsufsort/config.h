fad81f940141568a7f27180e6793a242cec8fe4a
Overmind <overmind@sd-132105.dedibox.fr>
2026-03-18 20:39:31 +0100

    fix: divsufsort MSVC compat — disable strings.h, use __inline
    Windows MSVC doesn't have <strings.h>. Also add divsufsort include
    path to shared lib target.

--- lib/external/divsufsort/config.h
+++ lib/external/divsufsort/config.h
@@ -4,8 +4,16 @@
 #define HAVE_STDLIB_H 1
 #define HAVE_MEMORY_H 1
 #define HAVE_STDDEF_H 1
-#define HAVE_STRINGS_H 1
 #define HAVE_INTTYPES_H 1
 #define HAVE_STDINT_H 1
+
+/* strings.h doesn't exist on MSVC */
+#ifdef _MSC_VER
+#define HAVE_STRINGS_H 0
+#define INLINE __inline
+#else
+#define HAVE_STRINGS_H 1
 #define INLINE inline
+#endif
+
 #define PROJECT_VERSION_FULL "2.0.1-embedded"
